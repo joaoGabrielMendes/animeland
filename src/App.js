e67c54fb06426d3ed61c9c52e89a7f9dabe66153
João Gabriel Mendes <jhonathammises@proton.me>
2023-01-10 20:37:19 -0700

    defining the structure of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./views/Home/Home";
+import Info from "./views/Info/Info";
+import NotFound from "./views/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/info" element={<Info />}></Route>
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   );
 }
-
 export default App;
---
+++ src/views/Home/Home.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Home;
