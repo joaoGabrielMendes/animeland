1a04637d25214748842ef26e4a54909eefcead16
João Gabriel Mendes <jhonathammises@proton.me>
2023-01-12 10:24:46 -0700

    drawing Slider component

---
+++ src/components/CarouselSlider/Slider.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SliderConfig = {
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 10,
+  slidesToScroll: 10,
+};
+
+function CarouselSlider() {
+  const [topAiring, setTopAiring] = useState([]);
+  const [topAiring2, setTopAiring2] = useState([]);
+  function getTopAiring() {
+    axios("https://gogoanime.consumet.org/top-airing").then((res) => {
+      setTopAiring(res.data);
+    });
+  }
+
+  function getTopAiring2() {
+    axios("https://gogoanime.consumet.org/top-airing?page=2").then((res) => {
+      setTopAiring2(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getTopAiring();
+    getTopAiring2();
+  }, []);
+
+  return (
+    <>
+      <Slider {...SliderConfig}>
+        {topAiring.map((iten) => (
+          <img alt="cover" src={iten.animeImg} />
+        ))}
+        {topAiring2.map((iten) => (
+          <img alt="cover" src={iten.animeImg} />
+        ))}
+      </Slider>
+    </>
+  );
+}
+
+export default CarouselSlider;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import CarouselSlider from "../../components/CarouselSlider/Slider";
 
 function Home() {
   return (
     <>
       <Header />
+      <CarouselSlider />
     </>
   );
 }
